e4331ee944b9eaf75f69180cd7c7de2011cf1d3e
James Wong <jwong_li@hotmail.com>
2019-10-13 13:42:30 -0700

    implement add/remove/re-order functionalities to To-Do List

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -6,7 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  items: any[];
+  currentItem = "";
 
-  constructor() {}
+  constructor() {
+    this.items = [ "Finish Project 007", "Study for Angular final", "Do groceries", "Go to the gym"];
+  }
+
+  addItem(item){
+    const text = item.trim();
+
+    if(text !== "") {
+      this.items.push(text);
+      this.currentItem = "";
+    }
+  }
+
+  removeItem(index) {
+    this.items.splice(index, 1);
+  }
+
+  reorderItems(event) {
+    const itemToMove = this.items.splice(event.detail.from, 1)[0];
+    this.items.splice(event.detail.to, 0, itemToMove);
+    event.detail.complete();
+  }
 
 }
